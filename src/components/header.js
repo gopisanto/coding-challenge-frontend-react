3cbc3bc341dfcab07a08c6b7412a881698ac7e5b
Santosh Kumar Gopisetty <gopi.santo@gmail.com>
2019-02-18 14:00:33 +0100

    build files

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
   <div className="header">
-    <img className="logo" alt="police" src="../screens/police.jpg" />
+    <img className="logo" alt="police" src="./police.jpg" />
     <div className="titles">
       <h1>Police Department Of Berlin</h1>
       <h3>Stolen Bykes</h3>
